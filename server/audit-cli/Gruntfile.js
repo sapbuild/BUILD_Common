449eaa02fd8f6b56c756c7f9682e0587609b2f53
David Jones <david.jones02@sap.com>
2015-09-10 13:24:16 +0100

    Further dep updates

--- server/audit-cli/Gruntfile.js
+++ server/audit-cli/Gruntfile.js
@@ -1,15 +1,12 @@
-var helper = require("norman-build-helper");
-
 module.exports = function (grunt) {
-    helper.setGrunt(grunt);
 
     grunt.initConfig({
         eslint: require("./build/grunt-eslint"),
         mochaTest: require("./build/grunt-mocha")
     });
-
-    helper.grunt.loadTasks("grunt-mocha-test");
-    helper.grunt.loadTasks("grunt-eslint");
+    
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-eslint');
 
     grunt.registerTask("default", [ "build" ]);
     grunt.registerTask("build", [ "eslint", "test" ]);
